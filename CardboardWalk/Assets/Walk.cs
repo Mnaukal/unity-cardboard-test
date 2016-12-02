70006808ee6ac61a0a0046dad70d968bc6b11151
Mnaukal <mnaukal@thetopfer.com>
2016-12-02 22:17:54 +0100

    Simple Walk with On/Off button

---
+++ CardboardWalk/Assets/Walk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+    public bool Walking = false;
+    public float speed = 0.01f;
+    public GameObject cameraObject;
+
+	public void StartStop(bool start)
+    {
+        Walking = start;
+    }
+
+    public void ToggleWalk()
+    {
+        Walking = !Walking;
+    }
+
+    private void FixedUpdate()
+    {
+        if(Walking)
+        {
+            transform.Translate(cameraObject.transform.forward * speed);
+        }
+    }
+}
